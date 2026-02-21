79f7f85118d69afd1aa8a2e5297fb2f62ebdea73
eugen <b-eugen@yandex.ru>
2026-02-21 20:26:08 +0300

    clear repository

--- scripts/profile.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# Пример профилирования (Linux, perf). Настройте под свою систему.
-cargo build --release
-perf record -g -a ./target/release/demo -- sleep 60 || true
-#perf report
-perf script > perf.script
-/home/eugen/RustroverProjects/module5-broken-app/stackcollapse-perf.pl perf.script | /home/eugen/RustroverProjects/module5-broken-app/flamegraph.pl > flame.svg
